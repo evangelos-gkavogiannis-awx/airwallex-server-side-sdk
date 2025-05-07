2a309a4e46410a6c4be3f9f3af5def0e9cc8a92d
evangelos-gkavogiannis-awx <evangelos.gkavogiannis@airwallex.com>
2025-05-07 22:22:04 +0100

    update the readme file

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Server-side SDKs (Beta)
+
+This an implemetation of the [Server-side SDKs (Beta)](https://staging.airwallex.com/docs/developer-tools__sdks__server-side-sdks-(beta))
+
+
 Steps to run it:
 1. Update the `.npmrc` with your gitlab token
 2. Update the `index.js` with a valid pair of `clientId` and `apiKey`
--- index.js
+++ index.js
@@ -2,22 +2,33 @@ import { Airwallex } from '@airwallex/node-sdk';
 
 // Initialize the client
 const client = new Airwallex({
-  clientId: '',     // replace with your actual Client ID
-  apiKey: '',         // replace with your actual API Key
-  env: 'demo',                    // or 'prod'
-  apiVersion: '2025-02-14',
-  timeout: 30000,
-  retry: 3,
+    clientId: '-SGVMBpwSdOfMw7Jxgt58g',     // replace with your actual Client ID
+    apiKey: '107a5c39a1b60c5a7671bd797355ff8994dd9ed5bc2fbca57d48c298927c467aabcd4f34651cae902798d5a0bca9dedf',         // replace with your actual API Key
+    env: 'demo',                    // or 'prod'
+    apiVersion: '2025-02-14',
+    timeout: 30000,
+    retry: 3,
 });
 
 async function main() {
-  try {
-    console.log(Object.keys(client)); //gives the supported modules
-    const response = await client.issuing.cards.getCards({card_status: 'ACTIVE'});
-    console.log('Global accounts:', response);
-  } catch (err) {
-    console.error('Error:', err);
-  }
+    try {
+        const paymentIntentRequest = {
+            amount: 10000,
+            currency: 'USD',
+            merchant_order_id: 'order_xyz_123',
+            request_id: 'req_id_abc3',
+            return_url: 'https://yourapp.com/payment-success',
+        };
+
+        console.log(Object.keys(client)); //gives the supported modules
+        const response = await client.issuing.cards.getCards({ card_status: 'ACTIVE' });
+        const PaResponse = await client.paymentAcceptance.paymentIntents.createPaymentIntent(paymentIntentRequest);
+        const PaResponse2 = await client.payouts.transfers
+        console.log('Global accounts:', response);
+        console.log(PaResponse)
+    } catch (err) {
+        console.error('Error:', err);
+    }
 }
 
 main();
